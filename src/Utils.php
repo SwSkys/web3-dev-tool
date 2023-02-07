2d742772db3a7cca2e09e8c383fa92b43d63796b
JokerMac1 <ali@ulovei.net>
2023-02-07 18:53:04 +0800

    Update cr

--- src/Utils.php
+++ src/Utils.php
@@ -7,6 +7,7 @@ use InvalidArgumentException;
 use kornrunner\Keccak;
 use phpseclib\Math\BigInteger as BigNumber;
 use stdClass;
+use Web3Tool\CryptoCurrencyPHP\Signature;
 
 class Utils
 {
@@ -732,6 +733,54 @@ class Utils
             $arr[$k] = self::hexToDec($hash);
         }
         return $arr;
+    }
+
+    function personal_ecRecover($msg, $signed) {
+        $personal_prefix_msg = "\x19Ethereum Signed Message:\n". strlen($msg). $msg;
+        $hex = Utils::keccak256($personal_prefix_msg);
+        return Utils::ecRecover($hex, $signed);
+    }
+
+    function ecRecover($hex, $signed) {
+        $rHex   = substr($signed, 2, 64);
+        $sHex   = substr($signed, 66, 64);
+        $vValue = hexdec(substr($signed, 130, 2));
+        $messageHex       = substr($hex, 2);
+        $messageByteArray = unpack('C*', hex2bin($messageHex));
+        $messageGmp       = gmp_init("0x" . $messageHex);
+        $r = $rHex;		//hex string without 0x
+        $s = $sHex; 	//hex string without 0x
+        $v = $vValue; 	//27 or 28
+
+        //with hex2bin it gives the same byte array as the javascript
+        $rByteArray = unpack('C*', hex2bin($r));
+        $sByteArray = unpack('C*', hex2bin($s));
+        $rGmp = gmp_init("0x" . $r);
+        $sGmp = gmp_init("0x" . $s);
+
+        if ($v != 27 && $v != 28) {
+            $v += 27;
+        }
+
+        $recovery = $v - 27;
+        if ($recovery !== 0 && $recovery !== 1) {
+            throw new Exception('Invalid signature v value');
+        }
+
+        $publicKey = Signature::recoverPublicKey($rGmp, $sGmp, $messageGmp, $recovery);
+        $publicKeyString = $publicKey["x"] . $publicKey["y"];
+
+        return '0x'. substr(Utils::keccak256(hex2bin($publicKeyString)), -40);
+    }
+
+    function strToHex($string)
+    {
+        $hex = unpack('H*', $string);
+        return '0x' . array_shift($hex);
+    }
+
+    function keccak256($str) {
+        return '0x'. Keccak::hash($str, 256);
     }
 
 }
